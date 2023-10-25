b84506eeb23ba070a97f82083940ca0d6ceba658
fahdaroua <fahd.aroua@gmail.com>
2023-10-25 20:46:53 +0200

    Complete Step One

--- react-client/src/components/AllCharacters.jsx
+++ react-client/src/components/AllCharacters.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import "./styles/AllCharacters.css";
+import React, { useState, useEffect } from "react";
 import Character from "./Character.jsx";
-function AllCharacters({data}) {
+import axios from "axios";
+
+function AllCharacters() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/get")
+      .then((response) => setCharacters(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="all-characters-container">
-      {data&& data.map((character, index) => (
+      {characters.map((character, index) => (
         <Character key={index} character={character} />
       ))}
     </div>
@@ -12,4 +21,3 @@ function AllCharacters({data}) {
 }
 
 export default AllCharacters;
-
